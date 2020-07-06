1dc92f26bf91c40621cd3be2037c9b3bbadc73ab
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-07-06 18:24:43 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios06Punto01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios06Punto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicios 06 Punto 01";
+        }
+    }
+}
